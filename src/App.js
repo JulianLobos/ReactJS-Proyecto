3f214d98868ce0ee02b795f9991a68544688cca0
Julian Lobos <julianign@gmail.com>
2022-06-04 16:26:58 -0300

    menu e-commerce

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Navbar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Jurold</h1>
-      <p>Black and white photography</p>
-      <p>by Julian Lobos</p>
+      <Navbar />
+      <h1 className='pageTitle'>jurold</h1>
+      <img src='jurold1.webp' alt='cloud and mountains' className='imageIndex'></img>
+      <p className='pageSubtitle'>Black and white photography</p>
+      <p className='pageAuthor'>by Julian Lobos</p>
     </div>
   );
 }
